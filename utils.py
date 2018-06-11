801cf3b4d5bbc8d25a7275bc38ef25b87b0f5d20
Zhengxu Xia <zxxia@ucla.edu>
2018-06-11 15:08:45 -0700

    Did some data cleaning work

---
+++ utils.py
@@ -0,0 +1,155 @@
+from datetime import timedelta, datetime
+from typing import List
+
+import numpy as np
+from cerebralcortex.core.datatypes.datastream import DataPoint
+
+
+# Sensor sample range
+MAX_LOC_LAT = 90.0
+MIN_LOC_LAT = -90.0
+MAX_LOC_LON = 180.0
+MIN_LOC_LON = -180.0
+MAX_LOC_ALT = 1000.0
+MIN_LOC_ALT = 0
+MAX_LOC_SPEED = 500.0
+MIN_LOC_SPEED = 0
+MAX_LOC_BEAR = 360.0
+MIN_LOC_BEAR = 0
+MAX_LOC_ACCUR = 100.0
+MIN_LOC_ACCUR = 0
+
+MAX_ACT_TYPE = 7
+MIN_ACT_TYPE = 0
+MAX_CONFIDENCE = 100.0
+MIN_CONFIDENCE = 0
+
+MAX_LIGHT_INTENSITY = 250.0
+MIN_LIGHT_INTENSITY = 0
+
+MAX_PROXIMITY = 10
+MIN_PROXIMITY = 0
+
+MAX_BATT_LEVEL = 100.0
+MIN_BATT_LEVEL = 0
+MAX_BATT_VOLT = 5000.0
+MIN_BATT_VOLT = 0
+MAX_BATT_TEMP = 100.0
+MIN_BATT_TEMP = -50.0
+
+MAX_BLE_DIS = 100.0
+MIN_BLE_DIS = 0
+
+MAX_BLE_RSSI = 100.0
+MIN_BLE_RSSI = -100.0
+
+MAX_BLE_TX = 100.0
+MIN_BLE_TX = -100.0
+
+MAX_STEP_CNT = 50.0
+MIN_STEP_CNT = 0
+
+
+def extract_matched_labels(labels: List, keywords: List)->List:
+    """
+    Extract sensor stream labels which contain all the keywords.
+    """
+    results = list()
+    for label in labels:
+        matched = True
+        for keyword in keywords:
+            matched = matched and (keyword in label)
+        if matched:
+            results.append(label)
+    return results
+
+
+def extract_all_data(CC, usr_id: str, stream_label: str)->List[DataPoint]:
+    """
+    Extract all sensory data with in a stream for a user.
+    """
+    data = list()
+    usr_streams = CC.get_user_streams(usr_id)
+    try:
+        target_stream = usr_streams[stream_label]
+    except KeyError:
+        print(usr_id + " does not have stream " + stream_label)
+        return data
+    # Enumerate stream id in target each  stream
+    for stream_id in target_stream['stream_ids']:
+        stream_days = CC.get_stream_days(stream_id)
+        # Get data for all stream days
+        for i, stream_day in enumerate(stream_days):
+            ds = CC.get_stream(stream_id, usr_id, stream_day)
+            data.extend(ds.data)
+    # Sort all datapoints in chronological order
+    data.sort()
+    return data
+
+
+
+def fill_missing_values(datapoints: List[DataPoint], freq: float) -> List[DataPoint]:
+    
+    """
+    Introperlate the datapoints based on assigned frequency.
+    """
+    
+    if not datapoints:
+        return datapoints
+    if freq == 0.0:
+        return datapoints
+    
+    # Convert frequency to time intveral in second.
+    time_interval = 1.0/freq
+    new_datapoints = list()
+    start_t  = datapoints[0].start_time
+    end_t = datapoints[-1].start_time
+    
+    # Create a new list of timestamps with adjacent timestamp separated by time_invertal.
+    t = start_t
+    new_ts = list()
+    
+    # Interpolate the data list
+    while t <= end_t:
+        new_ts.append(t)
+        t += timedelta(seconds=time_interval)
+    #print(start_t, end_t)
+    #print('# of new dp:', len(new_ts))
+
+    j = 0
+    for i in range(len(new_ts)):
+        if new_ts[i] >= datapoints[j].start_time:
+            new_datapoints.append(DataPoint(new_ts[i], None, datapoints[j].offset, datapoints[j].sample))
+            j += 1
+        else:
+            new_datapoints.append(DataPoint(new_ts[i], None, datapoints[j-1].offset, datapoints[j-1].sample))
+    return new_datapoints
+
+
+def dp_to_list(dp: DataPoint)-> List:
+    """
+    Convert DataPoint to a list.
+    """
+    result = list()
+    if dp is None:
+        return result
+    if dp.start_time is not None:
+        result.append(dp.start_time.timestamp())
+    else:
+        result.append(np.nan)
+        
+    if dp.end_time is not None:
+        result.append(dp.end_time.timestamp())
+    else:
+        result.append(np.nan)
+    result.append(dp.offset)
+    result.extend(list(dp.sample))
+    return result
+    
+
+def to_numpy_array(datapoints: List[DataPoint]):
+    """
+    Convert a list of DataPoints to numpy array.
+    """
+    data = [dp_to_list(dp) for dp in datapoints]
+    return np.array(data)
---
+++ validation.py
@@ -0,0 +1,240 @@
+'''
+This module provides functions to do data validation check.
+'''
+import numbers
+from typing import List
+from cerebralcortex.core.datatypes.datastream import DataPoint
+import utils
+
+def is_valid_phone_location(datapoint: DataPoint):
+    '''
+    Return True if input data point is a valid phone location data point.
+    Otherwise return False.
+    '''
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 6:
+        return False
+
+    val = datapoint.sample[0]
+    valid_lat = (isinstance(val, numbers.Real)
+                 and val >= utils.MIN_LOC_LAT
+                 and val <= utils.MAX_LOC_LAT)
+    val = datapoint.sample[1]
+    valid_lon = (isinstance(val, numbers.Real)
+                 and val >= utils.MIN_LOC_LON
+                 and val <= utils.MAX_LOC_LON)
+    val = datapoint.sample[2]
+    valid_alt = (isinstance(val, numbers.Real)
+                 and val >= utils.MIN_LOC_ALT
+                 and val <= utils.MAX_LOC_ALT)
+    val = datapoint.sample[3]
+    valid_speed = (isinstance(val, numbers.Real)
+                   and val >= utils.MIN_LOC_SPEED
+                   and val <= utils.MAX_LOC_SPEED)
+    val = datapoint.sample[4]
+    valid_bearing = (isinstance(val, numbers.Real)
+                     and val >= utils.MIN_LOC_BEAR
+                     and val <= utils.MAX_LOC_BEAR)
+    val = datapoint.sample[5]
+    valid_accuracy = (isinstance(val, numbers.Real)
+                      and val >= utils.MIN_LOC_ACCUR
+                      and val <= utils.MAX_LOC_ACCUR)
+
+    return (valid_lat
+            and valid_lon
+            and valid_alt
+            and valid_speed
+            and valid_bearing
+            and valid_accuracy)
+
+def is_valid_phone_activity_type(datapoint: DataPoint):
+    '''
+    Return True if input data point is a valid phone activity type data point.
+    Otherwise return False.
+    '''
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 2:
+        return False
+    val = datapoint.sample[0]
+    valid_type = (isinstance(val, numbers.Real)
+                  and val >= utils.MIN_ACT_TYPE
+                  and val <= utils.MAX_ACT_TYPE)
+    val = datapoint.sample[1]
+    valid_confidence = (isinstance(val, numbers.Real)
+                        and val >= utils.MIN_CONFIDENCE
+                        and val <= utils.MAX_CONFIDENCE)
+
+    return valid_type and valid_confidence
+
+def is_valid_phone_ambient_light(datapoint: DataPoint):
+    '''
+    Return True if input data point is a valid phone ambient light data point.
+    Otherwise return False.
+    '''
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 1:
+        return False
+
+    val = datapoint.sample[0]
+    return (isinstance(val, numbers.Real)
+            and val >= utils.MIN_LIGHT_INTENSITY
+            and val <= utils.MAX_LIGHT_INTENSITY)
+
+def is_valid_phone_proximity(datapoint: DataPoint):
+    '''
+    Return True if input data point is a valid phone proximity data point.
+    Otherwise return False.
+    '''
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 1:
+        return False
+
+    val = datapoint.sample[0]
+    return (isinstance(val, numbers.Real)
+            and val >= utils.MIN_PROXIMITY
+            and val <= utils.MAX_PROXIMITY)
+
+def is_valid_phone_battery(datapoint: DataPoint):
+    '''
+    Return True if input data point is a valid phone battery data point.
+    Otherwise return False.
+    '''
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 3:
+        return False
+    val = datapoint.sample[0]
+    valid_level = (isinstance(val, numbers.Real)
+                   and val >= utils.MIN_BATT_LEVEL
+                   and val <= utils.MAX_BATT_LEVEL)
+    val = datapoint.sample[1]
+    valid_volt = (isinstance(val, numbers.Real)
+                  and val >= utils.MIN_BATT_VOLT
+                  and val <= utils.MAX_BATT_VOLT)
+    val = datapoint.sample[2]
+    valid_temp = (isinstance(val, numbers.Real)
+                  and val >= utils.MIN_BATT_TEMP
+                  and val <= utils.MAX_BATT_TEMP)
+
+    return valid_level and valid_volt and valid_temp
+
+def is_valid_beacon(datapoint: DataPoint):
+    '''
+    Return True if input data point is a valid beacon data point.
+    Otherwise return False.
+    '''
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 3:
+        return False
+
+    val = datapoint.sample[0]
+    valid_dis = (isinstance(val, numbers.Real)
+                 and val >= utils.MIN_BLE_DIS
+                 and val <= utils.MAX_BLE_DIS)
+    val = datapoint.sample[1]
+    valid_rssi = (isinstance(val, numbers.Real)
+                  and val >= utils.MIN_BLE_RSSI
+                  and val <= utils.MAX_BLE_RSSI)
+    val = datapoint.sample[2]
+    valid_tx = (isinstance(val, numbers.Real)
+                and val >= utils.MIN_BLE_TX
+                and val <= utils.MAX_BLE_TX)
+
+    return valid_dis and valid_rssi and valid_tx
+
+def is_valid_step_count(datapoint: DataPoint):
+    if not isinstance(datapoint.sample, List) or len(datapoint.sample) != 1:
+        return False
+    
+    val = datapoint.sample[0]
+    valid_cnt = (isinstance(val, numbers.Real)
+                 and val >= utils.MIN_STEP_CNT
+                 and val <= utils.MAX_STEP_CNT)
+    return valid_cnt
+
+
+def validate_location(loc_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate phone location data stream
+    :param loc_data:
+    :return: valid_loc_data
+    '''
+    valid_loc_data = []
+    for datapoint in loc_data:
+        if is_valid_phone_location(datapoint):
+            valid_loc_data.append(datapoint)
+
+    return valid_loc_data
+
+def validate_activity_type(act_type_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate phone activity type data stream
+    :param act_type_data:
+    :return: valid_act_type_data
+    '''
+    if act_type_data is None:
+        return None
+    valid_act_type_data = []
+    for datapoint in act_type_data:
+        if is_valid_phone_activity_type(datapoint):
+            valid_act_type_data.append(datapoint)
+
+    return valid_act_type_data
+
+def validate_ambient_light(light_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate phone ambient light data stream
+    :param light_data:
+    :return: valid_light_data
+    '''
+    valid_light_data = []
+    for datapoint in light_data:
+        if is_valid_phone_ambient_light(datapoint):
+            valid_light_data.append(datapoint)
+
+    return valid_light_data
+
+def validate_proximity(proximity_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate phone proximity data stream
+    :param light_data:
+    :return: valid_proximity_data
+    '''
+    valid_proximity_data = []
+    for datapoint in proximity_data:
+        if is_valid_phone_proximity(datapoint):
+            valid_proximity_data.append(datapoint)
+
+    return valid_proximity_data
+
+def validate_battery(batt_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate phone battery data stream
+    :param batt_data:
+    :return: valid_batt_data
+    '''
+    valid_batt_data = []
+    for datapoint in batt_data:
+        if is_valid_phone_battery(datapoint):
+            valid_batt_data.append(datapoint)
+
+    return valid_batt_data
+
+def validate_beacon(ble_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate beacon data stream
+    :param ble_data:
+    :return: valid_ble_data
+    '''
+    valid_ble_data = []
+    for datapoint in ble_data:
+        if is_valid_beacon(datapoint):
+            valid_ble_data.append(datapoint)
+
+    return valid_ble_data
+
+def validate_step_count(step_cnt_data: List[DataPoint]) -> List[DataPoint]:
+    '''
+    validate step count data stream
+    :param step_cnt_data:
+    :return: valid_step_cnt_data
+    '''
+    valid_step_cnt_data = []
+    for datapoint in step_cnt_data:
+        if is_valid_step_count(datapoint):
+            valid_step_cnt_data.append(datapoint)
+
+    return valid_step_cnt_data
